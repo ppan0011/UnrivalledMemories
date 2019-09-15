fb415388b77c2a831c04cbc007c370a7a499983b
pooja <ppan0011@student.monash.edu>
2019-09-16 06:47:05 +1000

    Commit code

---
+++ RegistrationAndLoginP2/RegistrationAndLogin/Controllers/UserController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationAndLogin.Models;
+using System.Net.Mail;
+using System.Net;
+using System.Web.Security;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+       //Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        //Registration POST action 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified,ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+            //
+            // Model Validation 
+            if (ModelState.IsValid)
+            {
+                #region //Email is already Exist 
+                var isExist = IsEmailExist(user.EmailID);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exist");
+                    return View(user);
+                }
+                #endregion
+
+                #region Save to Database
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+
+                    Status = true;
+                }
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+
+        //Login 
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //Login POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin login, string ReturnUrl="")
+        {
+            string message = "";
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == login.EmailID).FirstOrDefault();
+                if (v != null)
+                {
+                    if (string.Compare(login.Password,v.Password) == 0)
+                    {
+
+                        // Logic for setting cookies
+                        int timeout = login.RememberMe ? 525600 : 20; // 525600 min = 1 year
+                        var ticket = new FormsAuthenticationTicket(login.EmailID, login.RememberMe, timeout);
+                        string encrypted = FormsAuthentication.Encrypt(ticket);
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                        cookie.Expires = DateTime.Now.AddMinutes(timeout);
+                        cookie.HttpOnly = true;
+                        Response.Cookies.Add(cookie);
+
+
+                        if (Url.IsLocalUrl(ReturnUrl))
+                        {
+                            return Redirect(ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        message = "Invalid credential provided";
+                    }
+                }
+                else
+                {
+                    message = "Invalid credential provided";
+                }
+            }
+            ViewBag.Message = message;
+            return View();
+        }
+
+        //Logout
+        [Authorize]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "User");
+        }
+
+
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+    }
+}
